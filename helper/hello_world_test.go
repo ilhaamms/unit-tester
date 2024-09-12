b0c664eec48f054d809a6bd6d1cca33fe2d52e84
Ilham Muhammad Sidiq <ilhaammuhammads@gmail.com>
2024-09-12 11:25:12 +0700

    learn until table test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -44,13 +45,81 @@ func TestHelloWorldSyifa(t *testing.T) {
 */
 
 func TestHelloWorldWithAssert(t *testing.T) {
-	result := HelloWorld("Hamzahh")
+	result := HelloWorld("Hamzah")
 	assert.Equal(t, "Hello Hamzah", result, "Result must be Hello Hamzah") // ini manggil yang t.Fail()
 	fmt.Println("TestHelloWorldWithAssert done")
 }
 
 func TestHelloWorldWithRequire(t *testing.T) {
-	result := HelloWorld("Silmii")
+	result := HelloWorld("Silmi")
 	require.Equal(t, "Hello Silmi", result, "Result must be Hello Silmi") // ini manggil yang t.FailNow()
 	fmt.Println("TestHelloWorldWithRequire done")
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("gabisa jalan di windows")
+	}
+
+	result := HelloWorld("Hamzah")
+	assert.Equal(t, "Hello Hamzah", result, "Result must be Hello Hamzah")
+}
+
+// fitur main yang nama functionya wajib TestMain
+// ini hanya akan dieksekusi sekali di package, bukan pertiap function unit test
+// jadi kalau beda package wajib bikin TestMain lagi
+func TestMain(m *testing.M) {
+	// before unit test
+	fmt.Println("Before unit test")
+
+	m.Run()
+
+	fmt.Println("After unit test")
+}
+
+// kalau mau test subTestnya doang pake go test -v -run=TestSubTest/NamaSubTest
+func TestSubTest(t *testing.T) {
+	t.Run("Ilham", func(t *testing.T) { // subTest dengan nama Ilham
+		result := HelloWorld("Ilham")
+		assert.Equal(t, "Hello Ilham", result, "Result must be Hello Ilham")
+	})
+
+	t.Run("Sidiq", func(t *testing.T) { // subTest dengan nama Sidiq
+		result := HelloWorld("Sidiq")
+		assert.Equal(t, "Hello Sidiq", result, "Result must be Hello Sidiq")
+	})
+}
+
+// TableTest
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct { // ini adalah anonymous struct
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			"Ilham",
+			"Ilham",
+			"Hello Ilham",
+		},
+		{
+			"Muhammad",
+			"Muhammad",
+			"Hello Muhammad",
+		},
+		{
+			"Sidiq",
+			"Sidiq",
+			"Hello Sidiq",
+		},
+	}
+
+	// ini namanya adalah table test, yaitu kita menyediakan data berupa slice yang berisi parameter dan ekspektasi hasil dari unit test
+	// lalu slice tersebut kita iterasi dengan sub test
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) { // sub test
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
 }
